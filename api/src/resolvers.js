89944e1b49a51bdc7d136cfcac7a7d156b644208
johnwongfc <wongjohn377@gmail.com>
2022-04-05 21:52:29 -0700

    added practice graphql

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,21 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_, __, ctx){
+
     }
   },
-  User: {
+  // Mutation: {
     
-  }
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+    
+  // }
 }
